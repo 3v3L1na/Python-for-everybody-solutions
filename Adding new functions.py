da7609979c00eb4c6ce427b5b60f8a4bd80d5710
Evelina Saponjic Jovanovic <119764859+3v3L1na@users.noreply.github.com>
2022-12-11 16:53:00 +0100

    Add files via upload

---
+++ Adding new functions.py
@@ -0,0 +1,19 @@
+def print_lyrics (): #def is a keyword that indicates that this is function definition. The name of this function is print_lyrics.
+    print ('Golden brown, texture like sun,')
+    print ('Lay me down, through my mind she runs')
+
+def repeat_lyrics ():
+    print_lyrics ()
+    print_lyrics 
+    
+def print_twice (bruce): 
+    print (bruce)
+    print (bruce)
+    
+    print_twice ('Spam')
+    
+    print_twice ('Spam' * 4)
+    
+    import math
+    print_twice (math.pi)
+    
